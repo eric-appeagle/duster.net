e8d31fccab540e6fcf21c5bfc7ae42c84552f371
Eric Solan <eric@informed.co>
2019-05-23 12:26:22 -0400

    First commit with all solution files

---
+++ Duster.Net.DustJsGenerator/DustCompiler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Jint;
+
+namespace Duster.Net
+{
+    public class DustCompiler
+    {
+        private static readonly Lazy<string> DustJs = new Lazy<string>(ValueFactory);
+
+        [SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
+        private static string ValueFactory()
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Duster.Net.dust-full-2.7.2.min.js"))
+            {
+                if (stream == null)
+                    throw new Exception("Couldn't get manifest resource stream for dust-full-2.7.2.min.js");
+
+                using (var reader = new StreamReader(stream))
+                    return reader.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Compiles dust template into JavaScript
+        /// </summary>
+        /// <param name="name">Template Name</param>
+        /// <param name="markup">Template Markup</param>
+        /// <returns>Compiled JavaScript</returns>
+        public static string Compile(string name, string markup)
+        {
+            return new Engine()
+                .Execute($"{DustJs.Value}dust.compile('{Sanitize(markup)}','{name}')")
+                .GetCompletionValue()
+                .ToString();
+        }
+
+
+        private static string Sanitize(string content)
+        {
+            var newLineStripped = Regex.Replace(content, @"[\n\t\r]", "", RegexOptions.Compiled);
+            var stripWhitespaceSafe = Regex.Replace(newLineStripped, @">\s+<", "><", RegexOptions.Compiled);
+
+            return stripWhitespaceSafe;
+        }
+    }
+}
---
+++ Duster.Net.Tests/TemplateCompilationTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Duster.Net.Tests
+{
+    [TestClass]
+    public class TemplateCompilationTests
+    {
+        [TestMethod]
+        public void CanCompileHelloWorldTemplate()
+        {
+            const string helloWorldMarkup = "<div>Hello world!</div>";
+            var compiled = DustCompiler.Compile("hw", helloWorldMarkup);
+            Assert.AreEqual("(function(dust){dust.register(\"hw\",body_0);function body_0(chk,ctx){return chk.w(\"<div>Hello world!</div>\");}body_0.__dustBody=!0;return body_0}(dust));", compiled);
+        }
+
+		[TestMethod]
+		public void CanCompileMultiLineTemplate()
+		{
+			const string helloWorldMarkup = "<div>\r\n\t\tHello\t world!\r\n</div>";
+			var compiled = DustCompiler.Compile("hw", helloWorldMarkup);
+			Assert.AreEqual("(function(dust){dust.register(\"hw\",body_0);function body_0(chk,ctx){return chk.w(\"<div>Hello world!</div>\");}body_0.__dustBody=!0;return body_0}(dust));", compiled);
+		}
+    }
+}
